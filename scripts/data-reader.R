60ceb44beb63474c08e087ee8fe005cf706d1812
jasonpcasey <jazzerj@gmail.com>
2015-05-22 09:01:07 -0400

    add reader

---
+++ scripts/data-reader.R
@@ -0,0 +1,16 @@
+# 
+# Read NSF Data From Web
+# Restructure for DB Load
+#
+# Jason P. Casey
+# 22MAY2015
+# First Revision
+#
+
+# Load Libraries
+library(reshape2)     #For data transpositions
+library(plyr)         #For aggregation and iterative changes to records
+library(ggplot2)      #For quick plots and diagnostics
+
+
+
