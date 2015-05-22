a5a1c381dd2908c23149cd85266b9f3660e74753
jasonpcasey <jazzerj@gmail.com>
2015-05-22 09:06:10 -0400

    added addresses to reader

--- scripts/data-reader.R
+++ scripts/data-reader.R
@@ -12,5 +12,9 @@ library(reshape2)     #For data transpositions
 library(plyr)         #For aggregation and iterative changes to records
 library(ggplot2)      #For quick plots and diagnostics
 
-
+# URL's to public release files from NSF
+specs <- c('http://www.nsf.gov/statistics/herd/data/exp2013.csv',
+           'http://www.nsf.gov/statistics/herd/data/exp2012.csv',
+           'http://www.nsf.gov/statistics/herd/data/exp2011.csv',
+           'http://www.nsf.gov/statistics/herd/data/exp2010.csv')
 
