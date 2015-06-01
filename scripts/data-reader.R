5cb38cd108c0dac79c21f1738ff3ccbfbff4d922
jasonpcasey <jazzerj@gmail.com>
2015-06-01 09:10:01 -0400

    update reader

--- scripts/data-reader.R
+++ scripts/data-reader.R
@@ -13,8 +13,18 @@ library(plyr)         #For aggregation and iterative changes to records
 library(ggplot2)      #For quick plots and diagnostics
 
 # URL's to public release files from NSF
-specs <- c('http://www.nsf.gov/statistics/herd/data/exp2013.csv',
-           'http://www.nsf.gov/statistics/herd/data/exp2012.csv',
-           'http://www.nsf.gov/statistics/herd/data/exp2011.csv',
-           'http://www.nsf.gov/statistics/herd/data/exp2010.csv')
+data.specs <- data.frame(year=c(2013L,2012L,2011L,2010L),
+                         url=c('http://www.nsf.gov/statistics/herd/data/exp2013.csv',
+                               'http://www.nsf.gov/statistics/herd/data/exp2012.csv',
+                               'http://www.nsf.gov/statistics/herd/data/exp2011.csv',
+                               'http://www.nsf.gov/statistics/herd/data/exp2010.csv'))
 
+# Read the data
+# dat <- read.table(file=data.specs$url[1],
+#                   header=TRUE,
+#                   sep=",",
+#                   quote="\"",
+#                   skip=0,
+#                   row.names=NULL,
+#                   stringsAsFactors=FALSE,
+#                   fileEncoding="utf-8")
